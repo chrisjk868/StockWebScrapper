d2257346ef1b249342553d4bdcb0a1571f01ef06
chrisjk868 <chrisjk868@gmail.com>
2021-07-25 00:32:29 +0800

    Get Tickers And Stocks Info

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-import requests
-from bs4 import BeautifulSoup
+import openpyxl as xl
+import yfinance as yf
+import datetime as dt
+
+
+def getTickers():
+    ws = xl.load_workbook('HK_2021.xlsx', data_only=True).active
+    processTickers = dict()
+    for row in range(2, ws.max_row):
+        current_cell = "{}{}".format('B', row)
+        processTickers[ws[current_cell].value + ' ' + current_cell] = 0
+    return processTickers
+
+
+def getStockPrices():
+    tickerSymbols = [*getTickers()]
+    for i in range(0, len(tickerSymbols)):
+        tickerSymbols[i] = tickerSymbols[i].split(' ')[0]
+    stocksInfo = yf.Tickers(tickerSymbols)
+    print(stocksInfo)
 
 def main():
-    URL = "https://www.geeksforgeeks.org/data-structures/"
-    r = requests.get(URL)
-    soup = BeautifulSoup(r.content, 'html5lib')
-    print(soup.prettify())
+    getStockPrices()
+
 
 if __name__ == '__main__':
     main()
