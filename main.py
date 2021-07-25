e1ebef12a5eecc08db10e39b884abbd2fb5187fa
chrisjk868 <chrisjk868@gmail.com>
2021-07-25 18:56:06 +0800

    Finished scrapper for all sheets

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ import yfinance as yf
 import pandas as pd
 
 
-def getTickers(ws, col):
-    processTickers = dict()
-    print(ws.max_row)
-    for row in range(2, ws.max_row):
+def get_tickers(ws, col):
+    process_tickers = dict()
+    max_row = max((c.row for c in ws['B'] if c.value is not None))
+    for row in range(2, max_row + 1):
         current_cell = "{}{}".format(col, row)
-        processTickers[ws[current_cell].value] = ['C' + current_cell[1:], 0]
-    return processTickers
+        process_tickers[ws[current_cell].value] = ['C' + current_cell[1:], 0]
+    return process_tickers
 
 
-def getStockPrices(stocks):
+def get_stock_prices(stocks):
     pd.set_option('display.max_columns', None)
-    tickerSymbols = [*stocks]
-    stocksInfo = yf.Tickers(tickerSymbols)
-    currentPrice = stocksInfo.history(period='1d')['Close'][0:3].to_numpy()[0]
-    return currentPrice
+    ticker_symbols = [*stocks]
+    stocks_info = yf.Tickers(ticker_symbols)
+    current_price = stocks_info.history(period='1d')['Close'][0:].to_numpy()[0]
+    return current_price
 
 
 def updateStockPrices(stockIndex, close):
@@ -35,13 +35,25 @@ def writeExcel(ws, stockIndex):
 
 def main():
     wb = xl.load_workbook('HK_2021.xlsx', data_only=True)
-    ws = wb.active
-    stockDict = getTickers(ws, 'B')
-    closingPrices = getStockPrices(stockDict)
-    updatedIndex = updateStockPrices(stockDict, closingPrices)
-    print(updatedIndex)
-    writeExcel(ws, updatedIndex)
-    wb.save('HK_2021.xlsx')
+    worksheet_names = wb.sheetnames[1:]
+    print(worksheet_names)
+    for sheet_index in range(1, 4):
+        wb.active = sheet_index
+        print(wb.active.title)
+        stock_dict = get_tickers(wb.active, 'B')
+        closing_prices = get_stock_prices(stock_dict)
+        updated_index = updateStockPrices(stock_dict, closing_prices)
+        print(updated_index)
+        writeExcel(wb.active, updated_index)
+        wb.save('HK_2021.xlsx')
+
+    # Recovery Code:
+    # ws = wb.active
+    # stock_dict = get_tickers(ws, 'B')
+    # closing_prices = get_stock_prices(stock_dict)
+    # updated_index = updateStockPrices(stock_dict, closing_prices)
+    # writeExcel(ws, updated_index)
+    # wb.save('HK_2021.xlsx')
 
 
 if __name__ == '__main__':
